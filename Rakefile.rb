4994f91af93749cb54c2eb0e1083c250e53c5f8a
Michael Trommer <mictro@gmail.com>
2011-01-07 20:39:52 +0100

    cleaned

--- Rakefile.rb
+++ Rakefile.rb
@@ -31,50 +31,6 @@ task :test do
   #   pg.data    = "asd"
   # end
   #p wiki.data
-
-  
-  # p page.sha
-  # p page.link(:version)
-  # h=page.history.first
-  # p h.link(:version)
-  # h=h.history.first
-  # p h.link(:version)
-  # h=h.history.first
-  # p h.link(:version)
-
-  
-  # p page.sha
-  # p page.link(:version)
-  # p page = page.history.first
-  # p page.sha
-  # p page.link(:version)
-
-  # puts
-  # pp page.history.first
-  # puts 
-  # pp page.data
-  # pp page.author
-  # pp page.date
-  # pp page.message
-  # pp page.extension
-
-  # pp page.repos
-
-  # puts
-
-  # page.history.each do |cm|
-  #   pp cm
-  # end
-  # pp page.path
-
-  # pp page.has_parent?
-  
-  # ga = GitAccess.new
-  # p ga.blob("7bb3f5590a1894613a80a11b62a47b0b33f14c90")
-  #p ga.blob(page.id).class
-  # p page.repos
-  # p page.repos.git
-  # p page.repos.git.git
   
 end
 
