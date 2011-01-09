c24f6c027bc2c2464b908a669fc25ac2b570c185
Michael Trommer <mictro@gmail.com>
2011-01-09 20:09:42 +0100

    call to rspec not via sh

---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+#
+#
+# Author:  Michael 'entropie' Trommer <mictro@gmail.com>
+#
+
+require "rubygems"
+require "rspec"
+
+require "lib/oy"
+
+include OY
+OY.path = "/tmp/testrepos"
+
+
+
+=begin
+Local Variables:
+  mode:ruby
+  fill-column:70
+  indent-tabs-mode:nil
+  ruby-indent-level:2
+End:
+=end
