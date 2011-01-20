90938851f53b3ea08d230a6893083820558021dc
Michael Trommer <mictro@gmail.com>
2011-01-20 18:51:37 +0100

    TOC added

--- lib/oy/markup/nokogiri.rb
+++ lib/oy/markup/nokogiri.rb
@@ -12,10 +12,29 @@ module OY
       def self.is_virtual?
         true
       end
+
+      def make_toc
+        toc = ::Nokogiri::HTML::DocumentFragment.parse "<div id='oy-toc'><h2>Table Of Contents</h2><ul></ul></div>"
+        i = 0
+        @doc.children.each do |child|
+          if child.name =~ /^h[123456]$/
+            child["id"] = "topic#{i+=1}"
+            li = ::Nokogiri::XML::Node.new("li", toc)
+            a  = ::Nokogiri::XML::Node.new("a", toc)
+            a["href"] = "##{child["id"]}"
+            a.content = child.text
+            a.parent = li
+            li['class'] = "#{child.name}"
+            toc.at_css("ul").add_child(li)
+          end
+        end
+        @doc.children.before toc
+      end
       
       def to_html
         @doc = ::Nokogiri::HTML::DocumentFragment.parse(data)
         parse_result
+        make_toc
         @doc.to_html
       end
 
