ef536738a074104929e511f5d5cd5170eb2444c2
Michael Trommer <mictro@gmail.com>
2011-01-31 19:14:47 +0100

    new indent offset

--- app/public/js/oy.js
+++ app/public/js/oy.js
@@ -1,105 +1,107 @@
 (function( $ ){
 
-   $.fn.setupFontselLinks = function(){
-     $("li", this).each(function(){
-       $(this).click(function(){
-         $("#dynfont").attr("href", "/css/font?font=" + escape($(this).text()));
-       });
-     });
-   };
+    $.fn.setupFontselLinks = function(){
+        $("li", this).each(function(){
+            $(this).click(function(){
+                $("#dynfont").attr("href", "/css/font?font=" + escape($(this).text()));
+            });
+        });
+    };
 
-   $.fn.setupFontsel = function(){
-     $(this).click(function(){
-       var box = $(this);
-       if($("#oy-font-list").length && $("#oy-font-list").is(":visible"))
-         $("#oy-font-list").slideUp(function(){ $(this).remove(); });
-       else {
-         $.ajax({
-           type: "GET",
-           url: "/oy/special/list_fonts",
-           success: function(data){
-             $(box).parent().append(data);
-           },
-           complete: function(){
-             $("#oy-font-list").setupFontselLinks();
-             $("#oy-font-list").slideDown();
-           }
-         });
-        }
-     });
-   };
+    $.fn.setupFontsel = function(){
+        $(this).click(function(){
+            var box = $(this);
+            if($("#oy-font-list").length && $("#oy-font-list").is(":visible"))
+                $("#oy-font-list").slideUp(function(){ $(this).remove(); });
+            else {
+                $.ajax({
+                    type: "GET",
+                    url: "/oy/special/list_fonts",
+                    success: function(data){
+                        $(box).parent().append(data);
+                    },
+                    complete: function(){
+                        $("#oy-font-list").setupFontselLinks();
+                        $("#oy-font-list").slideDown();
+                    }
+                });
+            }
+        });
+    };
 
-   $.fn.setupToc = function() {
-     var toc = $(this);
-     $("h2", toc).click(function(){
-       $("h2", toc).toggleClass("active");
-       $("ul", toc).fadeToggle();
-     });
-   };
+    $.fn.setupToc = function() {
 
-   $.fn.setupPreviewLink = function() {
-     $(this).click(function(){
-       var data = $("#oy-editform form").serialize();
-       $("#oy-editform-more").slideUp();
-       $("#oy-editform .spinner").show();
+        $.fn.setupToc = function() {
+            var toc = $(this);
+            $("h2", toc).click(function(){
+                $("h2", toc).toggleClass("active");
+                $("ul", toc).fadeToggle();
+            });
+        };
 
-       $.ajax({
-         type: "POST",
-         url:  "/preview",
-         data: data,
-         success: function(data){
-           $("#oy-preview-box").html(data);
-         },
-         complete: function(){
-           $("#oy-editform .spinner").fadeOut();
-           $("#oy-preview-box").slideDown();
+        $.fn.setupPreviewLink = function() {
+            $(this).click(function(){
+                var data = $("#oy-editform form").serialize();
+                $("#oy-editform-more").slideUp();
+                $("#oy-editform .spinner").show();
 
-           $("#oy-page .oy-preview-buttons .edit").click(function(){
-             $("#oy-preview-box").slideUp(function(){
-               $("#oy-editform-more").slideDown();
-             });
-           });
-         }
-       });
-       return false;
-     });
-   };
+                $.ajax({
+                    type: "POST",
+                    url:  "/preview",
+                    data: data,
+                    success: function(data){
+                        $("#oy-preview-box").html(data);
+                    },
+                    complete: function(){
+                        $("#oy-editform .spinner").fadeOut();
+                        $("#oy-preview-box").slideDown();
 
-   $.fn.setupGraphs = function() {
-     $("table.chart").each(function(){
-       var chartType   = $(this).attr("data-charttype");
-       var chartHeight = $(this).attr("data-height");
-       var chartWidth  = $(this).attr("data-width");
-       var chartVTitle = $(this).attr("data-v-title");
-       var chartHTitle = $(this).attr("data-h-title");
+                        $("#oy-page .oy-preview-buttons .edit").click(function(){
+                            $("#oy-preview-box").slideUp(function(){
+                                $("#oy-editform-more").slideDown();
+                            });
+                        });
+                    }
+                });
+                return false;
+            });
+        };
 
-       $(this).gvChart({
-         chartType: chartType,
-         gvSettings: {
-           vAxis: {title: chartVTitle},
-           hAxis: {title: chartHTitle},
-           width: chartWidth,
-           height: chartHeight
-         }
-       });
-     });
-   };
+        $.fn.setupGraphs = function() {
+            $("table.chart").each(function(){
+                var chartType   = $(this).attr("data-charttype");
+                var chartHeight = $(this).attr("data-height");
+                var chartWidth  = $(this).attr("data-width");
+                var chartVTitle = $(this).attr("data-v-title");
+                var chartHTitle = $(this).attr("data-h-title");
 
-
-})(jQuery);
-
-
-$(document).ready(function () {
-  $("#oy-fontsel").setupFontsel();
-
-  if($("table.chart").length) $("#oy-body").setupGraphs();
-  if($("#oy-toc").length) $("#oy-toc").setupToc();
-
-  if($(".oy-preview #preview").length){
-    $("#preview").setupPreviewLink();
-  }
+                $(this).gvChart({
+                    chartType: chartType,
+                    gvSettings: {
+                        vAxis: {title: chartVTitle},
+                        hAxis: {title: chartHTitle},
+                        width: chartWidth,
+                        height: chartHeight
+                    }
+                });
+            });
+        };
 
 
-});
+    })(jQuery);
+
+
+ $(document).ready(function () {
+     $("#oy-fontsel").setupFontsel();
+
+     if($("table.chart").length) $("#oy-body").setupGraphs();
+     if($("#oy-toc").length) $("#oy-toc").setupToc();
+
+     if($(".oy-preview #preview").length){
+         $("#preview").setupPreviewLink();
+     }
+
+
+ });
 
 
