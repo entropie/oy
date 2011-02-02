7a4d758d9b6a81cae3ea37e51ec8d9fdfec1d07d
Michael Trommer <mictro@gmail.com>
2011-02-02 22:44:57 +0100

    fix

--- app/public/js/oy.js
+++ app/public/js/oy.js
@@ -30,78 +30,77 @@
     };
 
     $.fn.setupToc = function() {
+        var toc = $(this);
+        $("h2", toc).click(function(){
+            $("h2", toc).toggleClass("active");
+            $("ul", toc).fadeToggle();
+        });
+    };
 
-        $.fn.setupToc = function() {
-            var toc = $(this);
-            $("h2", toc).click(function(){
-                $("h2", toc).toggleClass("active");
-                $("ul", toc).fadeToggle();
-            });
-        };
+    $.fn.setupPreviewLink = function() {
+        $(this).click(function(){
+            var data = $("#oy-editform form").serialize();
+            $("#oy-editform-more").slideUp();
+            $("#oy-editform .spinner").show();
 
-        $.fn.setupPreviewLink = function() {
-            $(this).click(function(){
-                var data = $("#oy-editform form").serialize();
-                $("#oy-editform-more").slideUp();
-                $("#oy-editform .spinner").show();
+            $.ajax({
+                type: "POST",
+                url:  "/preview",
+                data: data,
+                success: function(data){
+                    $("#oy-preview-box").html(data);
+                },
+                complete: function(){
+                    $("#oy-editform .spinner").fadeOut();
+                    $("#oy-preview-box").slideDown();
 
-                $.ajax({
-                    type: "POST",
-                    url:  "/preview",
-                    data: data,
-                    success: function(data){
-                        $("#oy-preview-box").html(data);
-                    },
-                    complete: function(){
-                        $("#oy-editform .spinner").fadeOut();
-                        $("#oy-preview-box").slideDown();
-
-                        $("#oy-page .oy-preview-buttons .edit").click(function(){
-                            $("#oy-preview-box").slideUp(function(){
-                                $("#oy-editform-more").slideDown();
-                            });
+                    $("#oy-page .oy-preview-buttons .edit").click(function(){
+                        $("#oy-preview-box").slideUp(function(){
+                            $("#oy-editform-more").slideDown();
                         });
-                    }
-                });
-                return false;
+                    });
+                }
             });
-        };
+            return false;
+        });
+    };
 
-        $.fn.setupGraphs = function() {
-            $("table.chart").each(function(){
-                var chartType   = $(this).attr("data-charttype");
-                var chartHeight = $(this).attr("data-height");
-                var chartWidth  = $(this).attr("data-width");
-                var chartVTitle = $(this).attr("data-v-title");
-                var chartHTitle = $(this).attr("data-h-title");
+    $.fn.setupGraphs = function() {
+        alert(1);
+        $("table.chart").each(function(){
+            var chartType   = $(this).attr("data-charttype");
+            var chartHeight = $(this).attr("data-height");
+            var chartWidth  = $(this).attr("data-width");
+            var chartVTitle = $(this).attr("data-v-title");
+            var chartHTitle = $(this).attr("data-h-title");
 
-                $(this).gvChart({
-                    chartType: chartType,
-                    gvSettings: {
-                        vAxis: {title: chartVTitle},
-                        hAxis: {title: chartHTitle},
-                        width: chartWidth,
-                        height: chartHeight
-                    }
-                });
+            $(this).gvChart({
+                chartType: chartType,
+                gvSettings: {
+                    vAxis: {title: chartVTitle},
+                    hAxis: {title: chartHTitle},
+                    width: chartWidth,
+                    height: chartHeight
+                }
             });
-        };
+        });
+    };
 
 
-    })(jQuery);
+})(jQuery);
 
 
- $(document).ready(function () {
-     $("#oy-fontsel").setupFontsel();
+$(document).ready(function () {
+    $("#oy-fontsel").setupFontsel();
 
-     if($("table.chart").length) $("#oy-body").setupGraphs();
-     if($("#oy-toc").length) $("#oy-toc").setupToc();
+    if($("table.chart").length) $("#oy-body").setupGraphs();
+    if($("#oy-toc").length) $("#oy-toc").setupToc();
 
-     if($(".oy-preview #preview").length){
-         $("#preview").setupPreviewLink();
-     }
+    if($(".oy-preview #preview").length){
+        $("#preview").setupPreviewLink();
+    }
 
 
- });
+});
 
 
